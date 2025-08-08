2f4bba5d3c6161b14bc9c448ffa2bfd8bf50e9a4
Tijana Lazendić <lazendictijana22@gmail.com>
2025-08-08 12:28:08 +0200

    feat/added-rental-price-configuration-manager

---
+++ urban-motion/src/main/java/net/etfbl/ip/urban_motion/controller/RentalPriceConfigController.java
@@ -0,0 +1,4 @@
+package net.etfbl.ip.urban_motion.controller;
+
+public class RentalPriceConfigController {
+}
---
+++ urban-motion/src/main/java/net/etfbl/ip/urban_motion/repository/RentalPriceConfigRepository.java
@@ -0,0 +1,2 @@
+package net.etfbl.ip.urban_motion.repository;public interface RentalPriceConfigRepository {
+}
