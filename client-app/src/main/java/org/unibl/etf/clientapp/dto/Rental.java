99e312f393c41f9a201e980eb057e666d28ebd98
Tijana Lazendić <lazendictijana22@gmail.com>
2025-08-14 14:23:54 +0200

    added all dtos

---
+++ client-app/src/main/java/org/unibl/etf/clientapp/dto/Rental.java
@@ -0,0 +1,24 @@
+package org.unibl.etf.clientapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Rental {
+    private Long id;
+    private LocalDateTime dateTime;
+    private Integer durationSeconds;
+    private Double startX;
+    private Double startY;
+    private Double endX;
+    private Double endY;
+    private Long clientId;
+    private String vehicleId;
+    private Double price;
+    private boolean active;
+}
