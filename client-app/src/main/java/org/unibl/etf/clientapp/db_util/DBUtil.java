ba59a72d0ada77ba0da4a9884dd98be562c68dc7
Tijana Lazendić <lazendictijana22@gmail.com>
2025-08-14 13:56:30 +0200

    added db config

---
+++ client-app/src/main/java/org/unibl/etf/clientapp/db_util/DBUtil.java
@@ -0,0 +1,67 @@
+package org.unibl.etf.clientapp.db_util;
+
+
+import java.sql.*;
+
+public class DBUtil {
+
+    private static ConnectionPool connectionPool = ConnectionPool.getInstance();
+
+    public static PreparedStatement prepareStatement(Connection c, String sql,
+                                                     boolean retGenKeys, Object... values) throws SQLException {
+
+        PreparedStatement ps = c.prepareStatement(sql,
+                retGenKeys ? Statement.RETURN_GENERATED_KEYS
+                        : Statement.NO_GENERATED_KEYS);
+
+        for (int i = 0; i < values.length; i++)
+            ps.setObject(i + 1, values[i]);
+
+        return ps;
+    }
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Connection c = connectionPool.checkOut();
+        return c;
+    }
+
+    public static void close(Connection c) {
+        if (c != null)
+            connectionPool.checkIn(c);
+    }
+
+    public static void close(Statement s) {
+        if (s != null)
+            try {
+                s.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+    }
+
+    public static void close(ResultSet rs) {
+        if (rs != null)
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+    }
+
+    public static void close(ResultSet rs, Statement s, Connection c) {
+        close(rs);
+        close(s);
+        close(c);
+    }
+
+    public static void close(Statement s, Connection c) {
+        close(s);
+        close(c);
+    }
+}
